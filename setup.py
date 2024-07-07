5a1cd18e840f26daa7f927304fcaa9c12e4efe2f
Gahan Rakholia <gahan94rakh@gmail.com>
2024-07-08 01:37:10 +0530

    gitignore built artifacts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="kubeconf",
-    version="dev",
+    version="0.1.0-dev",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
