9e348db9353e13e639c32d0a6a7cae220ee2a4ee
Gahan Rakholia <gahan94rakh@gmail.com>
2024-07-08 00:38:46 +0530

    add xdg config home filepath

--- kubeconf/__main__.py
+++ kubeconf/__main__.py
@@ -1,9 +1,21 @@
 import importlib.machinery
+import os
 
 from .searcher import Searcher
 from .settings import app_settings
 
-user_settings = importlib.machinery.SourceFileLoader("user_settings", "examples/kubeconf_settings").load_module()
+
+def user_settings_path():
+    config_dir = os.getenv("XDG_CONFIG_HOME") or os.path.join(os.path.expanduser("~"))
+    filename = "kubeconf_settings" if os.getenv("XDG_CONFIG_HOME") else ".kubeconf_settings"
+    config_path = os.path.join(config_dir, filename)
+    if os.path.exists(config_path):
+        return config_path
+
+
+overrides = user_settings_path()
+if overrides:
+    importlib.machinery.SourceFileLoader("user_settings", user_settings_path()).load_module()
 
 config = app_settings[-1]()  # get the last loaded settings from decorator
 Searcher(config).execute()
